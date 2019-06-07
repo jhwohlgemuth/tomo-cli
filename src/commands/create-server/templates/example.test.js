03998ab8ee246724536036f67955fb79c672de02
Jason Wohlgemuth <jhwohlgemuth@gmail.com>
2019-06-07 10:22:16 +0000

    feat: Enhance server example test

--- src/commands/create-server/templates/example.test.js
+++ src/commands/create-server/templates/example.test.js
@@ -1,11 +1,22 @@
 const request = require('supertest');
 const server = require('../server');
+const graphql = require('../graphql');
 
 const STATUS_OK = 200;
 
-describe('Test the root path', () => {
-    test('It should response the GET method', async () => {
-        const response = await request(server).get('/');
-        expect(response.statusCode).toBe(STATUS_OK);
+describe('HTTP Server', () => {
+    test('load root path', async () => {
+        const {statusCode} = await request(server).get('/');
+        expect(statusCode).toBe(STATUS_OK);
+    });
+    test('load example markdown file', async () => {
+        const {statusCode} = await request(server).get('/example.md');
+        expect(statusCode).toBe(STATUS_OK);
+    });
+});
+describe('GraphQL Server', () => {
+    test('load root path', async () => {
+        const {statusCode} = await request(graphql).get('/');
+        expect(statusCode).toBe(STATUS_OK);
     });
 });
